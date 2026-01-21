289936e514346358132f34aa69748b0c5eb14661
Zhenyuan-py <83067580+Zhenyuan-py@users.noreply.github.com>
2026-01-21 23:23:33 +0800

    First Upload file

---
+++ src/ingest/loader.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def load_csv(path : str) -> pd.DataFrame :
+    df = pd.read_csv (
+        path,
+        parse_dates=["InvoiceDate"],
+        dtype = {
+            "InvoiceNo" : "string",
+            "StockCode" : "string",
+            "Country"   : "string"
+        }
+    )
+
+    return df
---
+++ src/transforms/clean.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def clean_data(df : pd.DataFrame) -> pd.DataFrame :
+
+    df = df.copy()
+
+    #raise message to inform elt, drop for testing
+    df = df.dropna(subset = ['InvoiceNo', 'StockCode', 'UnitPrice', 'Quantity'])
+
+    df = df[df['UnitPrice'] > 0]
+
+    df['is_return'] = (df['StockCode'].str.startswith("C", na=False) | df['Quantity'] < 0)
+
+    df['line_gmv'] = df['UnitPrice'] * df['Quantity']
+
+    df['net_gmv'] = df['line_gmv'].where(~df['is_return'],0)
+
+    return df
